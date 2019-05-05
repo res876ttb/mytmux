aa40a88f1c3d6bf48acce662b5b728de1d0fc9c2
res876ttb <someone10851222@gmail.com>
2019-05-05 17:11:57 +0800

    Fix installation problem

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # backup original tmux configuration
-mv $HOME/.tmux.conf .tmux.conf.$(date "+%Y.%m.%d-%H:%M:%S")
-mv $HOME/.tmux.conf.func .tmux.conf.func.$(date "+%Y.%m.%d-%H:%M:%S")
+mv $HOME/.tmux.conf $HOME/.tmux.conf.$(date "+%Y.%m.%d-%H:%M:%S")
+mv $HOME/.tmux.conf.func $HOME/.tmux.conf.func.$(date "+%Y.%m.%d-%H:%M:%S")
 
 # clone from github
 git clone https://github.com/res876ttb/mytmux.git ~/.mytmux
