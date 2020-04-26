935c69e4efeeb0410ee0eec54f29ecf510fc8319
res876ttb <someone10851222@gmail.com>
2020-04-27 02:17:10 +0800

    Fix install script

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-rm -rf ~/.mytmux
-
 # backup original tmux configuration
 if [ -f $HOME/.tmux.conf ]; then
   echo Backup ~/.tmux.conf
-  mv $HOME/.tmux.conf $HOME/.tmux.conf.$(date "+%Y.%m.%d-%H:%M:%S")
+  cp $HOME/.tmux.conf $HOME/.tmux.conf.$(date "+%Y.%m.%d-%H:%M:%S")
+  rm ~/.tmux.conf
 fi
+
+rm -rf ~/.mytmux
 
 # clone from github
 git clone https://github.com/res876ttb/mytmux.git ~/.mytmux
