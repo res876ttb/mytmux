c996901301f0cf258b709c2149fad97e53a64542
res876ttb <someone10851222@gmail.com>
2024-10-06 23:06:37 +0800

    Update tmux config, using tpm, new theme

--- install.sh
+++ install.sh
@@ -2,16 +2,23 @@
 
 # backup original tmux configuration
 if [ -f $HOME/.tmux.conf ]; then
-  echo Backup ~/.tmux.conf
-  cp $HOME/.tmux.conf $HOME/.tmux.conf.$(date "+%Y.%m.%d-%H:%M:%S")
-  rm ~/.tmux.conf
+  backup_tmux_config=/tmp/.tmux.conf.$(date "+%Y.%m.%d-%H:%M:%S")
+  echo Backup $HOME/.tmux.conf to $backup_tmux_config
+  cp $HOME/.tmux.conf $backup_tmux_config
+  rm $HOME/.tmux.conf
 fi
 
-rm -rf ~/.mytmux
+rm -rf $HOME/.mytmux
+rm -rf $HOME/.tmux
 
-# clone from github
-git clone https://github.com/res876ttb/mytmux.git ~/.mytmux
+# clone tpm
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+
+# clone mytmux
+git clone https://github.com/res876ttb/mytmux.git $HOME/.mytmux
 
 # creaet symbolic link to $HOME
-ln -s ~/.mytmux/.tmux.conf $HOME/
+ln -s $HOME/.mytmux/.tmux.conf $HOME/
 
+# install plugins with tpm
+$HOME/.tmux/plugins/tpm/scripts/install_plugins.sh
